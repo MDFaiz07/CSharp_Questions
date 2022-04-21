c447dd0159b89fc951b1cdfe289cddbaf265dc59
admin <admin@CHETUIWK1144>
2022-04-21 14:26:59 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Console.WriteLine("Enter Element in a array");
+            for(int i=0;i<10;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int j = 0;  j< arr.Length; j++)
+            {
+                if(arr[j]%2==0)
+                {
+                    Console.WriteLine("Even"+arr[j]);
+                }
+                else
+                {
+                    Console.WriteLine("Odd"+arr[j]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
